3f834ad87bb54442226e0555c93ba6be395b32bf
dhanoosh9 <dhanoosh.donepudi@gmail.com>
2022-06-03 17:02:40 +0530

    Data Provider

--- src/test/java/testng/tasks/Provider.java
+++ src/test/java/testng/tasks/Provider.java
@@ -6,52 +6,31 @@ import java.util.List;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class Provider extends DynamicDDT2{
+
+
+public class Provider extends DynamicDDT2 {
 
 	@Test(dataProvider = "Data")
-	public void test123(String name, String email, String phone, String pan, String aadhar) {
-		System.out.println("Name: " + name+", Email: "+email+", Phone: "+phone+", PAN: "+pan+", Aadhar: "+aadhar);
-	}
-
-	@DataProvider
-	public Object[][] Data() {
-		List<UserTestData> data = new ArrayList<UserTestData>();
-		for (int i = 0; i < 10; i++) {
-			UserTestData user = new UserTestData();
-			user.setName(randomString(8));
-			user.setEmail(randomString(8) + "@gmail.com");
-			user.setPhone(randomnum(10));
-			user.setPan(randomString(4) + randomNum(4));
-			user.setAadhar(randomNum(12));
-
-			data.add(user);
-
-		}
-		// data.forEach(userdata -> {
-		// System.out.println("Name: "+userdata.getName());
-		// });
-		Object[][] testdata = null;
-
-		// for(int j=0;j<data.size();j++) {
-		// String name ;
-		// testdata.add(new Object[][] {
-		// {data.get(j)}
-		// });
-		// System.out.println(data.get(j));
-		// }
-		// data.forEach(userdata -> {
-		
-		for (UserTestData userdata : data) {
-			testdata = new Object[][] { { userdata.getName(), userdata.getEmail(), userdata.getPhone(),
-					userdata.getPan(), userdata.getAadhar() } };
-		}
-
-		// });
-		List<Object[][]> object = new ArrayList<>();
-		object.add(testdata);
-		return testdata;
+	public void test123(UserTestData user) {
+		System.out.println("Name: " + user.getName() + ", Email: " + user.getEmail() + ", Phone: " + user.getPhone()
+				+ ", PAN: " + user.getPan() + ", Aadhar: " + user.getAadhar());
 	}
 	
+	@DataProvider
+	public Object[][] Data() {
 
+		List<UserTestData> data = new ArrayList<UserTestData>();
+
+		for (int i = 0; i < 10; i++) {
+			data.add(new UserTestData(randomString(8), randomString(8) + "@gmail.com", randomnum(10),
+					randomString(4) + randomNum(4), randomNum(12)));
+
+		}
+
+		Object[][] testdata = { { data.get(0) }, { data.get(1) }, { data.get(2) }, { data.get(3) }, { data.get(4) },
+				{ data.get(5) }, { data.get(6) }, { data.get(7) }, { data.get(8) }, { data.get(9) } };
+
+		return testdata;
+	}
 
 }
--- src/test/java/testng/tasks/UserTestData.java
+++ src/test/java/testng/tasks/UserTestData.java
@@ -6,36 +6,54 @@ public class UserTestData {
 	public String phone;
 	public String pan;
 	public String aadhar;
+
+	public UserTestData(String name, String email, String phone, String pan, String aadhar) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.pan = pan;
+		this.aadhar = aadhar;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getPan() {
 		return pan;
 	}
+
 	public void setPan(String pan) {
 		this.pan = pan;
 	}
+
 	public String getAadhar() {
 		return aadhar;
 	}
+
 	public void setAadhar(String aadhar) {
 		this.aadhar = aadhar;
 	}
-		
 
 }
